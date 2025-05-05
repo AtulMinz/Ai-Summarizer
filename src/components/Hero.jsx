6dfc05302e6be68b237f0b4a76733b0d4f5d357d
Atulya Minz <atulminz.jsr@gmail.com>
2025-05-05 11:31:54 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/AtulMinz/Ai-Summarizer/", "_blank")
+            window.open("https://github.com/AtulMinz/Ai-Summarir/", "_blank")
           }
           className='black_btn'
         >
